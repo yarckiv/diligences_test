92ab80f359adfe7b90082f54adc820fd7d382c09
yarckiv <yarckiv@gmail.com>
2018-12-11 00:00:42 +0200

    initial

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Diligences project test",
+    "summary": "Custom report for testing task",
+    "version": "8.0.1.0.0",
+    "author": "Ivanusa",
+    "category": "Reporting",
+    "depends": [
+        'project',
+        'project_issue',
+    ],
+    "data": [
+        # "view/ir_actions_report_xml.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
